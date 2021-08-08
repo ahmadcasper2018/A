20e3f83a4f54d97d4d494ef527a0e47cb45b0346
casper <ahmadsy88@gmail.com>
2021-08-08 07:58:45 +0300

    Normalize data

--- skin-reloaded.py
+++ skin-reloaded.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 from PIL import Image
 
@@ -8,3 +7,6 @@ data = pd.read_csv('datasets/skin/hmnist_28_28_RGB.csv')
 other = pd.read_csv('datasets/skin/HAM10000_metadata.csv')
 target = data['label']
 input = data.drop(columns=['label'])
+
+input = np.array(input).reshape(-1, 28, 28, 3)
+input = input / 255.0
